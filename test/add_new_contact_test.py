2f42ac9c3c8979439515e9b05e3f560eda2070c7
Dmitriy Gaevskiy <bao.wyld@gmail.com>
2018-02-15 16:44:32 +0200

    Added fixture validation

--- test/add_new_contact_test.py
+++ test/add_new_contact_test.py
@@ -2,9 +2,7 @@ from model.contact import Contact
 
 
 def test_add_new_contact_test(app):
-    app.session.login(user="admin", password="secret")
     app.contact.create(Contact(firstname="Fname", middlename="Mname", lastname="Lname", company="TestCompany", address="TestAddress", homephone="111-11-11", mobilephone="222-22-22",
                                workphone="333-33-33", email="test@test.test", birth="1980", secondaryaddress="TestHomeAddress", secondaryhomenumber="444-44-44"))
-    app.session.logout()
 
 
--- test/add_new_group_test.py
+++ test/add_new_group_test.py
@@ -3,13 +3,9 @@ from model.group import Group
 
 
 def test_add_new_group(app):
-    app.session.login(user="admin", password="secret")
     app.group.create(Group(name="g1name", header="g1header", footer="g1footer"))
-    app.session.logout()
 
 
 def test_add_new_empty_group(app):
-    app.session.login(user="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
-    app.session.logout()
 
