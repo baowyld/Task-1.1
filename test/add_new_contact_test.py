e0ca5446073669f673faa6d2c45d8f2eedbbe709
Dmitriy Gaevskiy <bao.wyld@gmail.com>
2018-02-15 19:09:54 +0200

    Task #8 complete

--- test/add_new_contact_test.py
+++ test/add_new_contact_test.py
@@ -2,7 +2,9 @@ from model.contact import Contact
 
 
 def test_add_new_contact(app):
-    app.contact.create(Contact(firstname="Fname", middlename="Mname", lastname="Lname", nickname="Nick", title="TestTitle", company="TestCompany", address="TestAddress", homephone="111-11-11", mobilephone="222-22-22",
-                               workphone="333-33-33", email="test@test.test", birth="1980", secondaryaddress="TestHomeAddress", secondaryhomenumber="444-44-44"))
+    app.contact.create(Contact(firstname="Fname", middlename="Mname", lastname="Lname", nickname="Nick",
+                               title="TestTitle", company="TestCompany", address="TestAddress", homephone="111-11-11",
+                               mobilephone="222-22-22", workphone="333-33-33", email="test@test.test", birth="1980",
+                               secondaryaddress="TestHomeAddress", secondaryhomenumber="444-44-44"))
 
 
