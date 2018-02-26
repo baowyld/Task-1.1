f12779b480110a48037bcbf37e87041b72bb2ea6
Dmitriy Gaevskiy <bao.wyld@gmail.com>
2018-02-26 16:22:38 +0200

    Added get_group_list method and simple group list asserts

--- test/add_new_group_test.py
+++ test/add_new_group_test.py
@@ -3,9 +3,15 @@ from model.group import Group
 
 
 def test_add_new_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="g1name", header="g1header", footer="g1footer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 def test_add_new_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
