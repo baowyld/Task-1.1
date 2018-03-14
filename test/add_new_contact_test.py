70a8cf69f83d95b55e2bdc6309af3dda505cf212
Dmitriy Gaevskiy <bao.wyld@gmail.com>
2018-03-14 10:03:54 +0200

    Added parametrization for creating new groups\contacts (Task 15)

--- test/add_new_contact_test.py
+++ test/add_new_contact_test.py
@@ -1,13 +1,38 @@
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_new_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    # symbols = string.ascii_letters + string.digits + string. punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [
+    Contact(firstname="", middlename="",
+            lastname="", nickname="",
+            title="", company="", address="",
+            homephone="", mobilephone="", workphone="",
+            fax="", email="", email2="", email3="",
+            homepage="",
+            secondaryaddress="", secondaryphone="")] + [
+    Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 10),
+            lastname=random_string("lastname", 10), nickname=random_string("nickname", 10),
+            title=random_string("title", 10), company=random_string("company", 10), address=random_string("address", 10),
+            homephone=random_string("homephone", 5), mobilephone=random_string("mobilephone", 5),
+            workphone=random_string("workphone", 5), fax=random_string("fax", 5),
+            email=random_string("email", 10), email2=random_string("email2", 10), email3=random_string("email3", 10),
+            homepage=random_string("homepage", 10), secondaryaddress=random_string("address2", 10),
+            secondaryphone=random_string("secondaryphone", 10))
+    for i in range(3)
+]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_new_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Fname", middlename="Mname", lastname="Lname", nickname="Nick",
-                      title="TestTitle", company="TestCompany", address="TestAddress", homephone="1111111",
-                      mobilephone="2222222", workphone="3333333", fax="fax",
-                      email="test@test.test", email2="test2@test.test", email3="test3@test.test", homepage="homepage",
-                      birth="1980", secondaryaddress="TestAddress2", secondaryphone="4444444")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count_contacts()
     new_contacts = app.contact.get_contact_list()
