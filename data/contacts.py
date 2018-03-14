a7d56a3be67d095e128e6057da075b32ab6d3a08
Dmitriy Gaevskiy <bao.wyld@gmail.com>
2018-03-14 22:12:02 +0200

    Added test data generators (Task 18) Added test data load from json files (Task 19)

---
+++ data/contacts.py
@@ -0,0 +1,20 @@
+from model.contact import Contact
+
+testdata = [
+    Contact(firstname="firstname1", middlename="middlename1",
+            lastname="lastname1", nickname="nickname1",
+            title="title1", company="company1", address="address1",
+            homephone="homephone1", mobilephone="mobilephone1",
+            workphone="workphone1", fax="fax1",
+            email="email11", email2="email21", email3="email31",
+            homepage="homepage1", secondaryaddress="secondaryaddress1",
+            secondaryphone="secondaryphone1")
+    Contact(firstname="firstname2", middlename="middlename2",
+            lastname="lastname2", nickname="nickname2",
+            title="title2", company="company2", address="address2",
+            homephone="homephone2", mobilephone="mobilephone2",
+            workphone="workphone2", fax="fax2",
+            email="email12", email2="email22", email3="email32",
+            homepage="homepage2", secondaryaddress="secondaryaddress2",
+            secondaryphone="secondaryphone2")
+]
--- test/add_new_contact_test.py
+++ test/add_new_contact_test.py
@@ -1,37 +1,8 @@
 from model.contact import Contact
-import pytest
-import random
-import string
 
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits
-    # symbols = string.ascii_letters + string.digits + string. punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [
-    # Contact(firstname="", middlename="",
-    #         lastname="", nickname="",
-    #         title="", company="", address="",
-    #         homephone="", mobilephone="", workphone="",
-    #         fax="", email="", email2="", email3="",
-    #         homepage="",
-    #         secondaryaddress="", secondaryphone="")] + [
-    Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 10),
-            lastname=random_string("lastname", 10), nickname=random_string("nickname", 10),
-            title=random_string("title", 10), company=random_string("company", 10), address=random_string("address", 10),
-            homephone=random_string("homephone", 5), mobilephone=random_string("mobilephone", 5),
-            workphone=random_string("workphone", 5), fax=random_string("fax", 5),
-            email=random_string("email", 10), email2=random_string("email2", 10), email3=random_string("email3", 10),
-            homepage=random_string("homepage", 10), secondaryaddress=random_string("address2", 10),
-            secondaryphone=random_string("secondaryphone", 10))
-    for i in range(3)
-]
-
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_new_contact(app, contact):
+def test_add_new_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count_contacts()
